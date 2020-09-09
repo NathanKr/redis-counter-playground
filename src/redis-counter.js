1992d233a9f6de14f325e8a9cbdf33d892e9a9dc
Nathan Krasney <natankrasney@gmail.com>
2020-09-09 13:35:18 +0300

    first version

---
+++ src/redis-counter.js
@@ -0,0 +1,84 @@
+const redis = require("redis");
+const client = redis.createClient();
+
+const okResult = "OK";
+
+// --- increment by 1
+function increment(key) {
+    return new Promise((resolve, reject) => {
+      client.incr(key, (err, result) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+  
+        return resolve(result); // 
+      });
+    });
+  }
+
+function exist(key) {
+  return new Promise((resolve, reject) => {
+    client.exists(key, (err, result) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+
+      return resolve(result); // 1 on exist  , 0 on not exist 
+    });
+  });
+}
+
+// result is "OK" on success and error object on failure
+function setValueTo1(key, expiredInSec) {
+  return new Promise((resolve, reject) => {
+    client.setex(key, expiredInSec, 1, (err, result) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+
+      return resolve(result); // "OK"
+    });
+  });
+}
+
+// -- return number on success
+function get(key) {
+  return new Promise((resolve, reject) => {
+    client.get(key, (err, result) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+
+      const intNumber = parseInt(result, 10);
+      return resolve(intNumber);
+    });
+  });
+}
+
+function init() {
+  return new Promise((resolve, reject) => {
+    client.on("error", function (error) {
+      console.error(error);
+      return reject(error);
+    });
+
+    client.on("connect", function () {
+      console.log("Connected to Redis");
+      return resolve();
+    });
+  });
+}
+
+function finish() {
+  return new Promise((resolve, reject) => {
+    client.quit((result) => {
+      return resolve(result); // should be ?????
+    });
+  });
+}
+
+module.exports = { init, finish, setValueTo1, okResult, get , exist ,increment};
